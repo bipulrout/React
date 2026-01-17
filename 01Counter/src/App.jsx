1363a6df89d89ba633663e9bce0401dab55b34be
bipul <routbipul1@gmail.com>
2026-01-17 10:33:05 +0530

    Know about react Hooks

---
+++ 01Counter/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(5)
+  
+  // let counter = 10;
+
+  const addValue = () => {
+    if (counter === 20) {
+      return
+    } else {
+       // counter = counter + 1
+      setCounter(counter + 1)
+      console.log(counter)
+      console.log("Clicked", Math.random())
+    }
+   
+  }
+
+  const removeValue = () => {
+    if (counter > 0) {
+      setCounter(counter - 1)
+    }
+  }
+
+  return (
+    <>
+     
+      <h1>Hello React</h1>
+      <h2>Counter value: {counter}</h2>
+      <button onClick={addValue}>Increse Value {counter}</button>
+      <button onClick={removeValue}>Decrese Value {counter}</button>
+      <p>counter value is : {counter}</p>
+    </>
+  )
+}
+
+export default App
